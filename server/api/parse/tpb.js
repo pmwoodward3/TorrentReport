0575715cc4ae50a4e99046e1977aa21e1139d46f
Evans Stepanov <evans@stepanov.email>
2018-01-23 16:29:06 -0500

    trying pup fix

--- server/api/parse/tpb.js
+++ server/api/parse/tpb.js
@@ -3,15 +3,11 @@ const puppeteer = require('puppeteer')
 let scrape = async () => {
   const userAgent =
     'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.75.14 (KHTML, like Gecko) Version/7.0.3 Safari/7046A194A'
-  var browser
-  if (process.env.NODE_ENV !== 'production') {
-    browser = await puppeteer.launch({ headless: true })
-  } else {
-    browser = await puppeteer.launch({
-      headless: true,
-      args: ['--no-sandbox', '--disable-setuid-sandbox']
-    })
-  }
+  var browser = await puppeteer.launch({
+    headless: true,
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
+  })
+
   const page = await browser.newPage()
   page.setUserAgent(userAgent)
   await page.goto(
