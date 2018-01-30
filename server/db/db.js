d48592fc0ad271037d95e0d7c3c7255b5f2d2d5b
Evans Stepanov <evans@stepanov.email>
2018-01-30 17:39:27 -0500

    refactor connections

--- server/db/db.js
+++ server/db/db.js
@@ -1,9 +1,7 @@
-const Sequelize = require('sequelize')
-const db = new Sequelize(
-  process.env.DATABASE_URL || 'postgres://localhost:5432/TorrentReport',
-  {
-    logging: false,
-    operatorsAliases: false
-  }
-)
-module.exports = db
+const Sequelize = require('sequelize');
+
+const db = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost:5432/TorrentReport', {
+  logging: false,
+  operatorsAliases: false,
+});
+module.exports = db;
