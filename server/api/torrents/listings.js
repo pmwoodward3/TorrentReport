2481c08c2703b586b5e8e60b02beb10ca2ec8bfd
Evans Stepanov <evans@stepanov.email>
2018-02-03 22:04:07 -0500

    Setting up snapshot save

--- server/api/torrents/listings.js
+++ server/api/torrents/listings.js
@@ -19,3 +19,41 @@ router.get('/', (req, res, next) => {
   //   next()
   // }
 });
+
+router.get('/addinfos', (req, res, next) => {
+  // if (req.user && req.user.isAdmin) {
+  let listingObj;
+  let newInfoObj;
+  TorrentListing.findOrCreate({
+    where: {
+      name: 'Braven 2018 720p WEB-DL DD5 1 H264-FGT',
+    },
+  })
+    .spread((listing, created) => {
+      listingObj = listing;
+      return listing.getInfos({ scope: 'withSites' });
+    })
+    .then(infos => res.json(infos));
+
+  // .then((infos) => {
+  //   if(inf)
+  //   TorrentInfo.create({
+  //   uploadDate: new Date(),
+  //   uploadUser: 'some fake users',
+  //   size: '4.20 GB',
+  //   hash: '0123456789101112',
+  //   url: 'fakelink.com',
+  //   torrentGroupId: 1,
+  //   torrentListingId: listing.id,
+  // })})
+  // .then((newInfo) => {
+  //   newInfoObj = newInfo;
+  //   return listingObj.addInfo(newInfo);
+  // })
+  // .then(infos => res.json(infos))
+  // .catch(next);
+
+  // } else {
+  //   next()
+  // }
+});
