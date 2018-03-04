48896acf38020da426fca926010bad2b56e28335
Evans Stepanov <evans@stepanov.email>
2018-03-04 14:26:37 -0500

    closeStat method and test

--- server/reporterAgent/utils/index.spec.js
+++ server/reporterAgent/utils/index.spec.js
@@ -8,10 +8,39 @@ const expect = chai.expect;
 const { TorrentStats } = require('../../db/models');
 const db = require('../../db/db'); // db for forcesync
 const safeToRunAgent = require('./safeToRunAgent'); // part of test
-const { initStat } = require('./stats'); // part of test
+const { initStat, closeStat } = require('./stats'); // part of test
 
 describe('reporterAgent Utilities', () => {
-  describe('initStat()', () => {});
+  describe('initStat()', () => {
+    beforeEach(() => db.sync({ force: true }));
+    describe('when called', () =>
+      it('should create an active torrentStat listings', () =>
+        initStat()
+          .then(_ => TorrentStats.findAll({ where: { active: true } }))
+          .then((activeStats) => {
+            expect(Array.from(activeStats).length).to.equal(1);
+          })));
+  });
+  describe('closeStat()', () => {
+    beforeEach(() => db.sync({ force: true }));
+    describe('when called', () => {
+      it('should close active torrentStat listing ', () =>
+        initStat()
+          .then(_ => closeStat({}))
+          .then(_ => TorrentStats.findAll({ where: { active: true } }))
+          .then((activeStats) => {
+            expect(Array.from(activeStats).length).to.equal(0);
+          }));
+      it('should update closed listing info with passed object ', () =>
+        initStat()
+          .then(_ => closeStat({ siteCount: 21, scrapeCount: 21, infoCount: 21 }))
+          .then(_ =>
+            TorrentStats.findAll({ where: { siteCount: 21, scrapeCount: 21, infoCount: 21 } }))
+          .then((activeStats) => {
+            expect(Array.from(activeStats).length).to.equal(1);
+          }));
+    });
+  });
   describe('safeToRunAgent()', () => {
     beforeEach(() => db.sync({ force: true }));
     describe('when scrape is still active', () => {
--- server/reporterAgent/utils/stats.js
+++ server/reporterAgent/utils/stats.js
@@ -5,6 +5,27 @@ const initStat = () =>
     active: true,
   });
 
+const closeStat = statObj =>
+  TorrentStats.findOne({ where: { active: true } }).then((activeStat) => {
+    statObj.active = false;
+    return activeStat.update(statObj, {
+      fields: [
+        'siteCount',
+        'siteLoadCount',
+        'scrapeCount',
+        'torrentCount',
+        'torrentLoadCount',
+        'groupLoadCount',
+        'groupCount',
+        'snapshotCount',
+        'infoCount',
+        'active',
+        'endedAt',
+      ],
+    });
+  });
+
 module.exports = {
   initStat,
+  closeStat,
 };
