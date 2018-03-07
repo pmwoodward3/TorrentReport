bd855d99e37ba84b1d5a992a87b3788d7b25218b
Evans Stepanov <evans@stepanov.email>
2018-03-06 19:52:12 -0500

    added tests

--- server/reporterAgent/utils/safeToRunAgent.js
+++ server/reporterAgent/utils/safeToRunAgent.js
@@ -4,16 +4,20 @@ const safeToRunAgent = () =>
   // check if an active stat agent is running
   TorrentStats.findOne({ where: { active: true } })
     .then((result) => {
-      if (!result) return false;
+      if (result) return false;
       // check if enough time has elapsed since last scrape
-      return TorrentStats.max('id').then(maxId => TorrentStats.findById(maxId));
+      return TorrentStats.max('id');
     })
-    .then((latestStat) => {
-      const today = new Date();
-      const lastDate = new Date(latestStat.createdAt);
-      const timeDiff = lastDate.getTime() - today.getTime();
-      const diffDays = timeDiff / (1000 * 3600 * 22);
-      return diffDays <= -1;
+    .then((maxId) => {
+      if (maxId === false) return false;
+      if (!maxId) return true;
+      return TorrentStats.findById(maxId).then((latestStat) => {
+        const today = new Date();
+        const lastDate = new Date(latestStat.createdAt);
+        const timeDiff = lastDate.getTime() - today.getTime();
+        const diffDays = timeDiff / (1000 * 3600 * 22);
+        return diffDays <= -1;
+      });
     });
 
 module.exports = safeToRunAgent;
